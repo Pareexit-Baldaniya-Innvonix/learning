4129a0647534aad64335822901acb8fdda97d87e
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-01-08 16:27:50 +0530

    OOPs challange program

---
+++ test3.py
@@ -0,0 +1,21 @@
+# OOPs Program
+
+class Vehicle:
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+
+    def drive(self):
+        print(f"{self.brand} is driving at {self.speed} km/h.")
+
+class ElectricCar(Vehicle):
+    def __init__(self, brand, speed, battery):
+        super().__init__(brand, speed)
+        self.battery = battery
+
+    def silence_mode(self):
+        print(f"The {self.brand} is now driving silently because it has a {self.battery}% battery!")
+
+my_tesla = ElectricCar("Tesla", 120, 100)
+my_tesla.drive()
+my_tesla.silence_mode()
---
+++ test4.py
@@ -0,0 +1,22 @@
+# OOPs Program
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        super().__init__(self)
+        return "Meow!"
+
+animals = [Dog("Rex"), Cat("Whiskers")]
+
+for animal in animals:
+    print(f"{animal.name} says: {animal.speak()}")
