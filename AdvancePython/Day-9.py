afc133b98da587d2251ebf9875ba2b4edd9496d5
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-01-22 13:57:06 +0530

    modify file with .env database connection

--- AdvancePython/Day-9.py
+++ AdvancePython/Day-9.py
@@ -2,18 +2,38 @@
 
 # Task 2:
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
-conn = mysql.connector.connect(
-    user="root", host="localhost", passwd="", database="test"
-)
+load_dotenv()
 
-cursor = conn.cursor()
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
-records = """CREATE TABLE EMPLOYEE(
-NAME CHAR(20) NOT NULL,
-AGE INT,
-CITY CHAR(30)
-)"""
+try:
+    conn = mysql.connector.connect(
+        user=db_user, host=db_host, passwd=db_pass, database=db_name
+    )
 
-cursor.execute(records)
-conn.close()
+    if conn.is_connected():
+        print(f"Successfully connected with '{db_name}' database!")
+
+except mysql.connector.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    if "conn" in locals() and conn.is_connected():
+        conn.close
+
+# cursor = conn.cursor()
+
+# records = """CREATE TABLE EMPLOYEE(
+# NAME CHAR(20) NOT NULL,
+# AGE INT,
+# CITY CHAR(30)
+# )"""
+
+# cursor.execute(records)
+# conn.close()
