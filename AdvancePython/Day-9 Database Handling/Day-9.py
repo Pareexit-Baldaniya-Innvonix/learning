20b9522e214cdbcc65fd9fb41d50557f97ca1791
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-06 14:37:09 +0530

    modify Day-9 task according to comment

--- AdvancePython/Day-9 Database Handling/Day-9.py
+++ AdvancePython/Day-9 Database Handling/Day-9.py
@@ -1,37 +1,19 @@
-# Task 2:
 import mysql.connector
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+DB_INFO = "AdvancePython/Day-9 Database Handling/.env"
 
-db_host = os.getenv("DB_HOST")
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
+with open(DB_INFO) as f:
+    config = dict(
+        line.strip().split("=", 1)
+        for line in f
+        if "=" in line and not line.startswith("#")
+    )
+
 
 try:
     conn = mysql.connector.connect(
-        user=db_user, host=db_host, passwd=db_pass, database=db_name
+        host=config["DB_HOST"], user=config["DB_USER"], password=config["DB_PASSWORD"]
     )
-
-    if conn.is_connected():
-        print(f"Successfully connected with '{db_name}' database!")
-
-except mysql.connector.Error as e:
-    print(f"Error: {e}")
-
-finally:
-    if "conn" in locals() and conn.is_connected():
-        conn.close
-
-# cursor = conn.cursor()
-
-# records = """CREATE TABLE EMPLOYEE(
-# NAME CHAR(20) NOT NULL,
-# AGE INT,
-# CITY CHAR(30)
-# )"""
-
-# cursor.execute(records)
-# conn.close()
+    print(f"Connected successfully!")
+except KeyError as e:
+    print(f"Missing key in .env: {e}")
