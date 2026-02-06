38c17fd31152caf1f24add532fa636f11a8c5942
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-06 16:19:38 +0530

    update ORM.py file with .env file

--- AdvancePython/ORM/ORM.py
+++ AdvancePython/ORM/ORM.py
@@ -1,13 +1,29 @@
+import os
 from mongoengine import connect, Document, StringField
 
-connect(host = "mongodb://localhost:27017/my_database")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+if not all([host, port, db_name]):
+    raise ValueError("MONGO_URI not found! Ensure you started with F5.")
+
+connection_string = f"mongodb://{host}:{port}/{db_name}"
+
+print(f"--- Connecting to: {connection_string} ---")
+connect(host=connection_string)
+
 
 class User(Document):
-    name = StringField(required = True)
-    email = StringField(unique = True)
+    name = StringField(required=True)
+    email = StringField(unique=True)
 
-newUser = User(name = 'Virat', email = 'virat@india.com')
-newUser.save()
 
-for user in User.objects:
-    print(f"Found user: {user.name}")
+try:
+    users = User.objects
+    if not users:
+        print("Connected, but no users found in the collection.")
+    for user in users:
+        print(f"Found user: {user.name}")
+except Exception as e:
+    print(f"Connection failed: {e}")
